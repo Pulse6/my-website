77b9a6851f261056b0a7f979a9bd4c09e4bedb9d
Pulse6 <marco698698@hotmail.com>
2021-10-22 18:44:18 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <h1>This is an Home page</h1>
+    <div class="about">
+      <img src="@/assets/me.jpeg" alt="">
+      <h1>Nice to meet you <span role="img" aria-label="sheep">🐑</span></h1>
+      <p>My name is Hok Lun Siu, You may call me Marco. I am passionate about web development. I enjoy learning and sharing my own knowledge with my colleagues to hone our coding abilities, and problem solving. I am always looking forward to the new challenges of the ever changing technological world we live in. I am excited to be given the opportunity to be apart of a coding team.</p>
+    </div>
   </div>
 </template>
 
@@ -11,3 +14,30 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+.about {
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  /* grid-template-rows: repeat(3, 1fr); */
+  margin: 0.5rem;
+}
+
+h1 {
+  font-size: 2.5rem;
+  background-color: #FFE53B;
+  background: linear-gradient(147deg, #FFE53B 0%, #FF2525 74%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+p {
+  font-size: 1.5rem;
+}
+
+img {
+  max-width: 250px;
+  border-radius: 5%;
+}
+</style>
